fc90ca3ee70b42e296a4a95f4aff59aa321a4bc4
Maaz Khan <63423339+Maaz-R-Khan@users.noreply.github.com>
2025-04-08 10:28:14 -0400

    added methods for Graph class

--- src/main/java/org/example/Graph.java
+++ src/main/java/org/example/Graph.java
@@ -2,26 +2,49 @@ package org.example;
 
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class Graph {
 
     //a list of vertices
-    LinkedList<Integer> vertices = new LinkedList<>();
+   List<Integer> vertices;
 
     //a map of vertices to lists of edges.
     //has Integer as the key and a list of edges as the value.
-    HashMap<Integer, LinkedList<Integer>> edges = new HashMap<>();
+    Map<Integer, List<Edge>> adjList;
 
-    Graph() {
-        LinkedList<Integer> vertices = new LinkedList<>();
-        vertices.add(0);
-        edges.put(0, vertices);
+
+    public Graph() {
+        vertices = new LinkedList<>();
+        adjList = new HashMap<>();
     }
 
     void addVertex(int v) {
-        vertices.add(v);
+        vertices.add(v); //add a vertex to the vertex collection
+        adjList.put(v, new LinkedList<>()); //add a key/value pair for the adjacency list for this new vertex
+    }
+
+    void addEdge(int v1, int v2, int weight) {
+        Edge e = new Edge(v1, v2, weight);
+
+        //Get the adjacency lists.
+        List<Edge> v1AdjList = adjList.get(v1);
+        List<Edge> v2AdjList = adjList.get(v2);
 
 
+        // Add edge to adjacency lists.
+        v1AdjList.add(e);
+        v2AdjList.add(e);
+    }
+
+
+    void showGraph() {
+        System.out.println("Vertex List");
+        for (Integer v : vertices) {
+            System.out.print(v + " ");
+        }
+        System.out.println();
     }
 
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,15 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+    Graph g = new Graph();
+    g.addVertex(0);
+    g.addVertex(1);
+    g.addVertex(2);
 
+    g.addEdge(0,1,5);
+    g.addEdge(0,2,10);
+
+    System.out.println("Done!!!");
 
 
 
